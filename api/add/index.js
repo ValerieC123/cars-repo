23b1a7d9e77627da73d34aff57950209cd9668d3
ValerieC123 <102948171+ValerieC123@users.noreply.github.com>
2024-04-09 16:28:44 +0200

    adding the HTTPS get command to index.js in the add API

--- api/add/index.js
+++ api/add/index.js
@@ -2,4 +2,9 @@ module.exports = async function greetings (context, req) {
     context.res.json({
         text: "Hello from the API"
     });
+
+    
+    app.get('/cars', (req, res) => {
+        console.log(res);
+    });
 };
