f55b1603bb5510537201a12206b2e1c174b28272
ValerieC123 <102948171+ValerieC123@users.noreply.github.com>
2024-04-09 16:33:12 +0200

    adding the entire "get all cars" HTTP request to the add API

--- api/add/index.js
+++ api/add/index.js
@@ -2,9 +2,18 @@ module.exports = async function greetings (context, req) {
     context.res.json({
         text: "Hello from the API"
     });
+    const express = require('express');
+    const app = express();
+    app.use(express.json());
+    const PORT = process.env.PORT || 3001;
+    const cars = require('./cars.json');
 
-    
     app.get('/cars', (req, res) => {
         console.log(res);
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server started at http://localhost:${PORT}`);
+        
     });
 };
