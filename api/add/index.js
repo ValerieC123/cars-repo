f510516bbcdd426245ffe9605f83bbb5c87a5d28
ValerieC123 <102948171+ValerieC123@users.noreply.github.com>
2024-04-09 17:08:54 +0200

    A new button to check the API gets called

--- api/add/index.js
+++ api/add/index.js
@@ -1,19 +1,8 @@
-module.exports = async function greetings (context, req) {
+module.exports = async function greetingsAdd (context, req) {
     context.res.json({
-        text: "Hello from the API"
-    });
-    const express = require('express');
-    const app = express();
-    app.use(express.json());
-    const PORT = process.env.PORT || 3001;
-    const cars = require('./cars.json');
-
-    app.get('/cars', (req, res) => {
-        console.log(res);
-    });
-
-    app.listen(PORT, () => {
-        console.log(`Server started at http://localhost:${PORT}`);
+        text: "Hello from the Add API"
         
     });
+    
 };
+
